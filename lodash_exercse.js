a8499f9fc1fe5bcfc05cb44097147a74be1b9478
Aastha <aasthasarswat24@gmail.com>
2021-12-28 15:50:43 +0530

    lodash exercise commit-1

---
+++ lodash_exercse.js
@@ -0,0 +1,17 @@
+const _ = require("lodash");
+
+const members = [
+    {name: 'Laveesh Gupta', age: 20},
+    {name: 'Yash Jangid', age: 40},
+    {name: 'Firoz Khan', age: 41},
+    {name: 'Amrit Srivastava', age: 17},
+    {name: 'Chandraprakash Sharma'},
+    {name: 'Swpril Ahuja', age: 45},
+    {name: 'Yogesh Khatri', age: 51}
+];
+
+const fNames = _.forEach(members, (member)=>{
+    const splitName = member.name.split(" ");
+    const memberFname = splitName[0];
+    console.log(memberFname);
+})
